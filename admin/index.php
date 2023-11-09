758e3ddd66fa8ce137d45102bc488018eb5b6374
Prince Kumar <princek63055@gmail.com>
2023-11-09 17:14:45 +0530

    issues solved

--- admin/index.php
+++ admin/index.php
@@ -44,27 +44,31 @@ if(isset($_SESSION['username'])){
                         <?php  
                         if(isset($_POST['login'])){
                             include "config.php";
-                            $username = mysqli_real_escape_string($conn,$_POST['username']);
-                            $password = md5($_POST['password']);
-
-                         $sql = "SELECT user_id, username, role FROM user WHERE username = '{$username}' AND password = '{$password}'";
-                            // die();
-                            $result = mysqli_query($conn, $sql) or die("Query Failed.");
-                            if(mysqli_num_rows($result) > 0){
-                                while($row =  mysqli_fetch_assoc($result)){
-                                    session_start();
-                                    $_SESSION['username'] = $row['username'];
-                                    $_SESSION['user_id'] = $row['user_id'];
-                                    $_SESSION['user_role'] = $row['role'];
-
-                                    header("Location: {$hostname}/admin/post.php");
-                                }
-
+                            if(empty($_POST['username']) || empty($_POST['password'])){
+                                echo '<div class="alert alert-danger">All fields must be filled.</div>';
+                                die();
                             }else{
-                                echo '<div class="alert alert-danger">Username and Password are not matched.</div>';
-                            }
-                        }
+                                $username = mysqli_real_escape_string($conn,$_POST['username']);
+                                $password = md5($_POST['password']);
+    
+                                $sql = "SELECT user_id, username, role FROM user WHERE username = '{$username}' AND password = '{$password}'";
+                                // die();
+                                $result = mysqli_query($conn, $sql) or die("Query Failed.");
+                                if(mysqli_num_rows($result) > 0){
+                                    while($row =  mysqli_fetch_assoc($result)){
+                                        session_start();
+                                        $_SESSION['username'] = $row['username'];
+                                        $_SESSION['user_id'] = $row['user_id'];
+                                        $_SESSION['user_role'] = $row['role'];
+    
+                                        header("Location: {$hostname}/admin/post.php");
 
+                                    }
+                                }else{
+                                     echo '<div class="alert alert-danger">Username and Password are not matched.</div>';
+                                }
+                            }   
+                        }   
 
                         ?>
                     </div>
